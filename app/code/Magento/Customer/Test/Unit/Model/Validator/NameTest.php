8e9e58921c79f503f540e93edcc3fe5906618e32
in-session <info@in-session.de>
2024-10-31 06:10:38 +0100

    Update NameTest.php

--- app/code/Magento/Customer/Test/Unit/Model/Validator/NameTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/Validator/NameTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -74,7 +74,7 @@ class NameTest extends TestCase
     /**
      * @return array
      */
-    public function expectedPunctuationInNamesDataProvider(): array
+    public static function expectedPunctuationInNamesDataProvider(): array
     {
         return [
             [
