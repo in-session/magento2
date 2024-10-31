8c157795e667cc9ccc3637c4c06d7d75c07d2e42
in-session <info@in-session.de>
2024-10-31 06:08:38 +0100

    Update StreetTest.php

--- app/code/Magento/Customer/Test/Unit/Model/Validator/StreetTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/Validator/StreetTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -71,7 +71,7 @@ class StreetTest extends TestCase
      *
      * @return array
      */
-    public function expectedPunctuationInStreetDataProvider(): array
+    public static function expectedPunctuationInNamesDataProvider(): array
     {
         return [
             [
